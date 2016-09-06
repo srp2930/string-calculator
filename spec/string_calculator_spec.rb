bf3933afc464eb3f3bd186ee35210ef79d579549
srp2930 <s.rimingtonpounder@gmail.com>
2016-09-06 15:25:05 +0100

    test for different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,5 +24,9 @@ describe Calculator do
     it 'should return the correct sum when \n is included in the string of numbers' do
       expect(subject.add("1\n2,3")).to eq 6
     end
+
+    it 'should return correct sum with different delimiters' do
+      expect(subject.add("//;\n1;2")).to eq 3
+    end
   end
 end
