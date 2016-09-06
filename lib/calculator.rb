a36b06b04f5f2986d46cf3342c7a8909a6b38aed
srp2930 <s.rimingtonpounder@gmail.com>
2016-09-06 16:37:51 +0100

    refactored code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,23 @@
 class Calculator
+
   def add(string)
-    integer_array = string.gsub("\n", ',').split(',')
-    integer_array.map(&:to_i).reduce(0, :+)
+    amended_string = default_delimiters(string)
+    new_array = change_to_array(amended_string)
+    sum_array(new_array)
   end
+
+  private
+
+  def change_to_array(string)
+    string.split(',')
+  end
+
+  def default_delimiters(string)
+    string.gsub("\n", ',')
+  end
+
+  def sum_array(array)
+    array.map(&:to_i).reduce(0, :+)
+  end
+
 end
