26768b61f56168f2cc1cf2b31b3559feb08a3833
srp2930 <s.rimingtonpounder@gmail.com>
2016-09-06 17:38:10 +0100

    refactor code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,22 +3,27 @@ class Calculator
   def add(string)
     raise 'negatives not allowed' if string.include?('-')
     amended_string = default_delimiters(string)
-    new_array = change_to_array(amended_string)
-    sum_array(new_array)
+    new_array = convert_to_array(amended_string)
+    integer_array = convert_elements_to_integers(new_array)
+    sum_array(integer_array)
   end
 
   private
-
-  def change_to_array(string)
-    string.scan(/\w/)
-  end
 
   def default_delimiters(string)
     string.gsub("\n", ',')
   end
 
+  def convert_to_array(string)
+    string.scan(/\w/)
+  end
+
+  def convert_elements_to_integers(array)
+    array.map(&:to_i)
+  end
+
   def sum_array(array)
-    array.map(&:to_i).reduce(0, :+)
+    array.reduce(0, :+)
   end
 
 end
