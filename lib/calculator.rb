cec30a60daf95fdcd1a026004a895d025075495e
srp2930 <s.rimingtonpounder@gmail.com>
2016-09-07 15:21:35 +0100

    tests passing bar hyphen test

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,18 +4,22 @@ class Calculator
     raise 'negatives not allowed' if string.include?('-')
     new_array = convert_to_array(string)
     integer_array = convert_elements_to_integers(new_array)
-    # raise 'negatives not allowed' if integer_array.includes?
     sum_array(integer_array)
   end
 
   private
 
   def detect_delimiter(string)
-    string.scan(/(?<=\/\/).(?=\n)/)
+    string.scan(/(?<=\/\/).(?=\n)/).join
+  end
+
+  def remove_start(string)
+    string.gsub(/\/\/.\n/,"")
   end
 
   def convert_to_array(string)
-    string.scan(/\w+/)
+    string2 = remove_start(string)
+    string2.split(detect_delimiter(string))
   end
 
   def convert_elements_to_integers(array)
