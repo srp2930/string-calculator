bed5af92c46aa3aa6654d73f152851902c14a998
srp2930 <s.rimingtonpounder@gmail.com>
2016-09-07 15:00:43 +0100

    correct regex for delimiter

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -4,11 +4,15 @@ class Calculator
     raise 'negatives not allowed' if string.include?('-')
     new_array = convert_to_array(string)
     integer_array = convert_elements_to_integers(new_array)
-    raise 'negatives not allowed' if integer_array.includes?
+    # raise 'negatives not allowed' if integer_array.includes?
     sum_array(integer_array)
   end
 
   private
+
+  def detect_delimiter(string)
+    string.scan(/(?<=\/\/).(?=\n)/)
+  end
 
   def convert_to_array(string)
     string.scan(/\w+/)
